0ff21015332e2df3385cf8d90416bed6dc7e9711
CBVRT2008 <63355565+CBVRT2008@users.noreply.github.com>
2023-02-28 16:14:27 +0100

    Final push of the day

--- main/main.ino
+++ main/main.ino
@@ -1,20 +1,12 @@
-#include <SoftwareSerial.h>
 #include <SPI.h>
 #include <SD.h>
 
-
-SoftwareSerial barcodeSerial(9, 10); // RX, TX
 File sdLog;
 
-int DoNewLine = 1;
-
-String tempBarcode;
-String barcode;
+String fromFile;
 
 void setup() {
   Serial.begin(9600);
-  barcodeSerial.begin(9600);
-  delay(1000);
   Serial.println("pronto");
 
 //--------------------------------------
@@ -23,46 +15,38 @@ void setup() {
     Serial.println("initialization failed!");   //error check
   }
   
-  sdLog = SD.open("sdLog.txt");
+  sdLog = SD.open("gescandeBarcodes.txt");
   if (sdLog) {
-    Serial.println("sdLog.txt:");
+    Serial.println("gescandeBarcodes.txt:");
+  }
 
-  Serial.print("barcode.txt (sdLog.txt): ");
+  /*if (sdLog.available()){
+      sdLog.write("3800103433425");
+      Serial.println("Made new file and added barcode");
+      }*/
+    
+  
+  Serial.print("Inhoud gescandeBarcodes.txt: ");
     while (sdLog.available()) {
-      barcode = sdLog.read();
-      Serial.print(barcode.c_str());
-      for(const char* p=barcode.c_str(); *p; ) {
+      fromFile = sdLog.read();
+      Serial.print(fromFile.c_str());
+      for(const char* p=fromFile.c_str(); *p; ) {
         Serial.print(*p++);
   }
     }
     sdLog.close();
     }
-}
+
 
 
 void loop() {
-  sdLog = SD.open("sdLog.txt");
+  /*
+  sdLog = SD.open("gescandeBarcodes.txt");
   
-  /*while (sdLog.available()) {
-      barcode = sdLog.read();} */
-  
-  while (barcodeSerial.available()) {
-      barcode += Serial.println(barcodeSerial.read());
-      Serial.write("barcode: ");
-      Serial.println(barcode);
-      DoNewLine += 1;
-  }
-
-  if (DoNewLine > 13){    
-    Serial.println("\n");
-    Serial.println("barcode: ");
-    Serial.println(barcode.toInt());
-    barcode = "";
-    DoNewLine = 0;
-  }
-  
+  while (sdLog.available()) {
+      fromFile = sdLog.read();}
 
   if (sdLog) {
-    sdLog.println(barcode);
-    sdLog.close();}
+    sdLog.println(fromFile);
+    sdLog.close();}*/
 }
