b11a859157ea3ff58dd6be28b76c2e77cd4b4be4
CBVRT2008 <63355565+CBVRT2008@users.noreply.github.com>
2023-04-11 15:18:43 +0200

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -7,6 +7,28 @@ String barcode;
 String fromFile;
 SoftwareSerial ss(9, 8); // RX, TX
 
+//----------------------------------------
+
+void SDSchrijf(String string, String fileNaam){
+  File file;
+  file = SD.open(fileNaam, FILE_WRITE);
+  file.println(string);
+  file.close();
+  }
+
+String SDLees(String fileNaam){
+  File file;
+  String inhoud;
+  file = SD.open(fileNaam);
+  while (file.available()) {
+    //inhoud = inhoud + "x" + String(file.read());
+    inhoud = inhoud + file.read(); //https://cplusplus.com/reference/cstdlib/itoa/
+  }
+  file.close();
+  return inhoud;
+}
+
+//----------------------------------------
 
 void setup() {
   Serial.begin(9600);
@@ -20,21 +42,12 @@ void setup() {
     goto Retry;
   }
 
-  barcode = "test";
-  file = SD.open("a.txt", FILE_WRITE);
-  file.println(barcode);
-  file.close();
+  SDSchrijf("string", "a.txt");
+  
   Serial.println("Barcode naar file geprint");
 //---------------------------------------
 
-   file = SD.open("a.txt");
-
-  while (file.available()) {
-    Serial.write(file.read());
-  }
-  
-  file.close();
-  
+  Serial.println(SDLees("a.txt"));
 }
 
 void loop() {
